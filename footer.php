dff84c8394a097183e2c4ca324e1bc14b88cb666
EzraKenya <44658266+EzraKenya@users.noreply.github.com>
2019-02-05 11:10:28 +0300

    yes

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	 <footer><h2>Connect With Us</h2>
+		<p>
+	  <ul class="nav_menu">
+        <li><a href="https://www.facebook.com/djkezzy"><img src="images/fbs.jpg" width="50" height="50">Facebook</a></li>
+            <li><a href="about.html"><img src="images/Twitter.png" width="50" height="50">Twitter</a></li>
+            <li><a href="media.html"><img src="images/insta.jpg" width="50" height="50">Instagram</a></li>
+            <li><a href="https://www.instagram.com/vdjkezzy/"><img src="images/youtube.png" width="70" height="50">Youtube</a></li>
+            <li><a href="https://www.linkedin.com/in/ezra-kilonzo-74796b40/"><img src="images/Twitter.png" width="50" height="50">Linked In</a></li>
+     </ul>
+		<p>
+		&copy;Copyright Ezra</p></footer>
+</div>
+</body>
+</html>
+</body>
+</html>
+			?>
